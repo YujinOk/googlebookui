68a8afd7ad01add8cdc930342c8ca322769618be
YujinOk <72746448+YujinOk@users.noreply.github.com>
2021-10-05 23:18:09 +1100

    changed fetch, made them all functional, and changed overall styling

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,6 @@
-"strict";
-let bookData = [];
-const searchTerms = "murder";
-async function getBook() {
+"use strict";
+
+async function getBook(searchterms) {
     // 1. made googlebooks API key
     // 2. found the fetch code lines
     // 3. found a way to get array of data from the json
@@ -11,48 +10,28 @@ async function getBook() {
     // 7. how 'space' works in search terms?
     // 8. able to change/ display diff text via DOM but not outside of the {.then hell}
 
-    fetch(
+    const bookData = await fetch(
         "https://www.googleapis.com/books/v1/volumes?q=" +
-            searchTerms +
+            searchterms +
             "&key=AIzaSyDh0KfqPFEslm8mZInmTgAUbAY0KgSNsbs",
-    )
-        .then((response) => response.json())
-        .then((result) => {
-            // this.setState({ books: result.items })
-            // bookData.push(result);
-            // console.log(result.items);
-            bookData = result.items.map((book) => {
-                return book.volumeInfo.title;
-            });
-            return bookData;
-        })
-        .then((bookTitle) => {
-            console.log(bookTitle);
-            // document.querySelector("a").innerText = bookTitle[0];
-        });
+    );
+    const bookJson = await bookData.json();
+    return bookJson;
+    // .then((response) => respo nse.json())
+    // .then((result) => {
+    //     // this.setState({ books: result.items })
+    //     // bookData.push(result);
+    //     // console.log(result.items);
+    //     bookData = result.items.map((book) => {
+    //         return book.volumeInfo.title;
+    //     });
+    //     console.log(bookData);
+    //     return bookData;
+    // });
+    // .then((bookTitle) => {
+    //     console.log(bookTitle);
+    //     // document.querySelector("a").innerText = bookTitle[0];
+    // });
 }
-getBook();
-// fetch(
-//     "https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDh0KfqPFEslm8mZInmTgAUbAY0KgSNsbs",
-// )
-//     .then((response) => response.json())
-//     .then((result) => {
-//         // this.setState({ books: result.items })
-//         // bookData.push(result);
-//         // console.log(result.items);
-//         bookData = result.items.map((book) => {
-//             return book.volumeInfo.title;
-//         });
-//         return bookData;
-//     })
-//     .then((bookTitle) => {
-//         console.log(bookTitle);
-//     });
-console.log(bookData);
-// console.log(bookData);
-// const bookList = [];
-// bookList.push(bookData[0]);
-// console.log(bookList);
-// bookData.items.forEach((book) => {
-//     console.log(book.volumeInfo.authors);
-// });
+
+export { getBook };
