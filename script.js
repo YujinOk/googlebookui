2cce246d90e786b604dbb7fa0079bb7efc9227e2
YujinOk <72746448+YujinOk@users.noreply.github.com>
2021-10-11 01:46:55 +1100

    Added comments on script.js

--- script.js
+++ script.js
@@ -2,29 +2,31 @@
 let slideIndex = 1;
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides((slideIndex += n));
+function plusSlides(parameter) {
+    showSlides((slideIndex = parameter + slideIndex));
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides((slideIndex = n));
+function currentSlide(parameter) {
+    showSlides((slideIndex = parameter));
 }
 
-function showSlides(n) {
+function showSlides(parameter) {
     let i;
     let slides = document.getElementsByClassName("slides");
 
-    if (n > slides.length) {
+    if (parameter > slides.length) {
         slideIndex = 1;
     }
-    if (n < 1) {
+    //  in order to keep the slides within the length of slides array (6)
+    if (parameter < 1) {
         slideIndex = slides.length;
     }
+    // in order to hide previous image, preventing from displaying all images at once
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
+    // slides[5].style.display="block" => display!
     slides[slideIndex - 1].style.display = "block";
 }
 showSlides(slideIndex);
