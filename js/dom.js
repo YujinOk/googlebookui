efd4007376784e7b54fe5ee9679bdde63d56fc39
YujinOk <72746448+YujinOk@users.noreply.github.com>
2021-10-11 01:16:25 +1100

    Changed media query and added modal feature

--- js/dom.js
+++ js/dom.js
@@ -7,11 +7,12 @@ const thumbNailImg = document.querySelectorAll(".thumbnail__img");
 const searchBtn = document.querySelector("#navbar__search__Btn");
 const title = document.querySelectorAll(".title");
 const authors = document.querySelectorAll(".author");
-const description = document.querySelectorAll(".des");
+const modalBtn = document.querySelectorAll(".myModalBtn");
+const modalText = document.querySelectorAll(".myModalText");
 
 async function renderBook(searchTerms) {
     const infoBooks = await makeBookObj(searchTerms);
-    console.log(infoBooks);
+    console.log(infoBooks.description);
     slideImg.forEach((item, index) => {
         item.setAttribute("src", infoBooks.imglink[index].thumbnail);
         // Looping through the slideImg current item and index of that slideImg array - by accessing index of imglink array in order to grab the url from thumbnail
@@ -26,9 +27,24 @@ async function renderBook(searchTerms) {
     authors.forEach((book, index) => {
         book.innerText = `Author: ${infoBooks.authors[index]}`;
     });
+    modalBtn.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            if (modalText[index].innerText === "") {
+                console.log("hi");
+                modalText[index].innerText = `${infoBooks.description[index]}`;
+                btn.innerText = "❌";
+            } else {
+                modalText[index].innerText = "";
+                btn.innerText = "Click for more info...";
+            }
+        });
+    });
 }
 
 searchBtn.addEventListener("click", () => {
     const search = document.querySelector(".navbar__search").value;
     renderBook(search);
 });
+// descriptionBtn.addEventListener("click", () => {
+//     descriptionBtn.innerText = `${infoBooks.description[0]}`;
+// });
